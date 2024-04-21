115fb3776772977c10a253b98f60cf3a9d26f4ee
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-04-21 16:53:42 +0600

    ofcanves added

--- src/components/Nevbar.jsx
+++ src/components/Nevbar.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar()
 {
+    const [open,setOpen] = useState(false);
     return(
         <nav>
             <div className="left">
@@ -18,9 +20,9 @@ function Navbar()
                 <a href="" className="login">Sign in</a>
                 <a href="" className="register">Sign up</a>
                 <div className="menuIcon">
-                    <img src="/menu.png" alt="" />
+                    <img src="/menu.png" alt="" onClick={()=>setOpen((prev) => !prev)} />
                 </div>
-                <div className="menu">
+                <div className={ open ? "menu active" : "menu"}>
                     <a href="">Home</a>
                     <a href="">About</a>
                     <a href="">Contact</a>
